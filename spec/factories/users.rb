3d944234e9bee7e12e9b5512d9bcfd9e51759fc3
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-17 15:00:55 +0900

    修正しました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)+("")
+    password = Faker::Internet.password(number: 6, min_alpha: 1)
     password {password}
     password_confirmation {password}
     last_name {"クラウン"}
