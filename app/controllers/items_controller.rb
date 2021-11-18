b4f851b34f20fd9d0cc22275276eebcc27a86a50
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-18 12:16:07 +0900

    バルデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ before_action :authenticate_user!, only: [:new]
 
   private
   def item_params
-    paramas.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :user  )
+    paramas.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :user, :image).merge(user_id: current_user.id )
   end
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,16 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :product_description, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :pay_for_shipping_id, presence: true
-  validates :region_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :price, presence: true
-  validates :user , presence: true
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+
+  # validates :name, presence: true
+  # validates :product_description, presence: true
+  # validates :category_id, presence: true
+  # validates :state_id, presence: true
+  # validates :pay_for_shipping_id, presence: true
+  # validates :region_id, presence: true
+  # validates :shipping_date_id, presence: true
+  # validates :price, presence: true
+  # validates :user , presence: true
 end
