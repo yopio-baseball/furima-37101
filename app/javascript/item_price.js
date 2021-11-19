f55bd1a92cfac166c40a9f08ebd6f8729691fc3b
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-19 14:16:21 +0900

    販売手数料・販売価格表示機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+  
+const priceInput = document.getElementById("item-price")
+console.log(priceInput);
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+  console.log(addTaxDom);
+
+  const profitNumber = document.getElementById("profit")
+  const value_result = inputValue * 0.1
+  console.log(value_result)
+  profitNumber.innerHTML = (Math.floor(parseInt(inputValue) - parseInt(addTaxDom.innerHTML)));
+    console.log(profitNumber);
+});
+});
+
+
+
+
+
