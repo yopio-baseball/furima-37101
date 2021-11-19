0ca1bda1ab619f2148b961faa2743b1d27250e94
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-19 15:46:08 +0900

    商品出品完了後の遷移先

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,13 +10,19 @@ before_action :authenticate_user!, only: [:new]
   end
 
   def create
-    Item.create(item_params)
-    @items = Item.order("created_at DESC")
+    # Item.create(item_params)
+    # @items = Item.order("created_at DESC")
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path  
+    else
+      render :new
+    end 
   end
 
   private
   def item_params
-    paramas.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :user, :image).merge(user_id: current_user.id )
+    params.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :user, :image).merge(user_id: current_user.id )
   end
 end
 
