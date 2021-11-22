f82933ded6441581100a906ae4067f458b11eefe
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-22 16:32:24 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new]
-  def index
+before_action :authenticate_user!, only: [:new, :edit]
+  
+def index
     @items = Item.all.order(created_at: :desc)
    
   end
@@ -19,11 +20,31 @@ before_action :authenticate_user!, only: [:new]
       render :new
     end 
   end
+
   def show
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+      unless current_user.id == @item.user.id 
+        redirect_to root_path
+      end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+
+
   def item_params
     params.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :image, ).merge(user_id: current_user.id )
   end
