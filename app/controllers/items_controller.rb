2959a66a1ab9fadd269b8dc2617fd729685e37d2
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-22 17:02:02 +0900

    変更しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :edit]
+before_action :set_item, only: [:edit, :show, :update, ]
   
 def index
     @items = Item.all.order(created_at: :desc)
@@ -22,18 +23,18 @@ def index
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
       unless current_user.id == @item.user.id 
         redirect_to root_path
       end
   end
 
   def update
-    @item = Item.find(params[:id])
+   
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,7 +44,9 @@ def index
 
   private
 
-
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :product_description, :category_id, :state_id, :pay_for_shipping_id, :region_id, :shipping_date_id, :price, :image, ).merge(user_id: current_user.id )
