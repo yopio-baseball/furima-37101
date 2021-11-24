c48ab036ea29d5b0dc810b50e40e83e8a6183b44
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-24 11:42:20 +0900

    モデル作成

---
+++ db/migrate/20211124015059_create_addresses.rb
@@ -0,0 +1,16 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string   :post_code,         null: false
+      t.string   :region_id,         null: false
+      t.string   :address_city,      null: false
+      t.string   :address_street,    null: false
+      t.string   :address_building 
+      t.string   :phone_number,       null: false
+      t.references    :order,        null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
+
+
