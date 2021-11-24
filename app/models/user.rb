780576151916397db69397b7304f62e46be656e8
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-24 11:41:37 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  # has_many :orders
+  has_many :orders
 
   validates :nickname, presence:true
   validates :day_of_birth, presence:true 
