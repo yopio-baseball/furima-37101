25dd91b17871886888ea0dfbbabd821047291526
yopio-baseball <kota.0507.lotte@gmail.com>
2021-11-28 15:15:30 +0900

    修正しました

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :move_to_index, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
     @order_address = OrderAddress.new   
